dccd54fe5f92921cab17eae6ad304caeb61e40fc
Brad Hansen <brad.m.hansen@gmail.com>
2017-12-25 16:08:20 +1300

    Capitalised Tuple

--- Decoder.java
+++ Decoder.java
@@ -13,8 +13,8 @@ public class Decoder {
 		int i = 0;
 		int ref = 0;
 		byte data = 0;
-		tuple[] dictionary = new tuple[256];
-		tuple[] temp = dictionary;
+		Tuple[] dictionary = new Tuple[256];
+		Tuple[] temp = dictionary;
 
 		while ((i = input.read()) != -1) {
 			ref = ((byte) i & 0xFF) << 24 | ((byte) input.read() & 0xFF) << 16 | ((byte) input.read() & 0xFF) << 8
@@ -22,19 +22,19 @@ public class Decoder {
 			data = (byte) input.read();
 
 			if (dictionary.length == dictionaryIndex) { //if dictionary needs extending then double the length
-				temp = new tuple[dictionaryIndex * 2];
+				temp = new Tuple[dictionaryIndex * 2];
 				System.arraycopy(dictionary, 0, temp, 0, dictionaryIndex);
 				dictionary = temp;
 			}
 
 			// adds tuple to dictionary and then prints it and all its children
-			dictionary[dictionaryIndex] = new tuple(ref, data);
+			dictionary[dictionaryIndex] = new Tuple(ref, data);
 			outputData(dictionaryIndex, dictionary, output);
 			dictionaryIndex++;
 		}
 	}
 
-	public static void outputData(int refNum, tuple[] dict, PrintStream out) throws Exception {
+	public static void outputData(int refNum, Tuple[] dict, PrintStream out) throws Exception {
 		// calls itself until the reference is the root aka 0, and prints out all data on the path to the root
 		if (refNum != 0) {
 			outputData(dict[refNum].ref, dict, out);
--- tuple.java
+++ tuple.java
@@ -1,9 +1,9 @@
-public class tuple {
+public class Tuple {
 
 	int ref;
 	byte data;
 
-	public tuple(int r, byte d) {
+	public Tuple(int r, byte d) {
 		ref = r;
 		data = d;
 	}
