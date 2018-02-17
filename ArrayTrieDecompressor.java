ff1d5c9061d9174026d5f049883a5fed47c04236
Brad Hansen <brad.m.hansen@gmail.com>
2018-02-17 23:06:01 +1300

    Adds bitunpacking to the decompressor

--- ArrayTrieDecompressor.java
+++ ArrayTrieDecompressor.java
@@ -11,45 +11,62 @@ public class ArrayTrieDecompressor {
     int[] indexDictionary = new int[256];
     byte[] dataDictionary = new byte[256];
     int index = 1;
+    int encodeAnotherBitOnThisIndex = 2;
+    int bitsToEncode = 1;
     int backRef = 0;
     byte data = 0;
     Stack<Byte> byteSequence = new Stack<Byte>();
+    long bitBuffer = 0;
+    int bitBufferLen = 0;
+    long temp = 0;
 
     // while there are bytes to read
     for (int inputInt = System.in.read(); inputInt >= 0; inputInt = System.in.read()) {
-      // gather the int reference index and the new data byte
-      backRef = readInt(inputInt);
-      data = (byte) System.in.read();
-      // resize dictionary arrays if they are full
-      if (index == indexDictionary.length){
-        int[] tempArray = new int[indexDictionary.length*2];
-        System.arraycopy(indexDictionary, 0, tempArray, 0, indexDictionary.length);
-        indexDictionary = tempArray;
-        byte[] tempArray2 = new byte[dataDictionary.length*2];
-        System.arraycopy(dataDictionary, 0, tempArray2, 0, dataDictionary.length);
-        dataDictionary = tempArray2;
-      }
-      //save new backRef and data pair at the current index location
-      indexDictionary[index] = backRef;
-      dataDictionary[index++] = data;
-      // follow the sequence of indexes until it hits 0, 
-      // pushing each data byte from each index onto a stack
-      byteSequence.push(data);
-      while (backRef != 0) {
-        byteSequence.push(dataDictionary[backRef]);
-        backRef = indexDictionary[backRef];
-      }
-      // write from the byte stack until it is empty
-      while (!byteSequence.empty()) {
-        System.out.write(byteSequence.pop().byteValue());
+      // add them to the buffer
+      bitBuffer = bitBuffer | (long) inputInt << bitBufferLen;
+      bitBufferLen += 8;
+      // when the buffer contains the ref and data, extract them
+      if (bitBufferLen >= bitsToEncode + 8) {
+        // isolate the reference from the buffer
+        temp = (bitBuffer << 64 - bitsToEncode) >>> 64 - bitsToEncode;
+        backRef = (int) temp;
+        bitBuffer = bitBuffer >>> bitsToEncode;
+        bitBufferLen -= bitsToEncode;
+        // isolate the data byte from the buffer
+        temp = (bitBuffer << 64 - 8) >>> 64 - 8;
+        data = (byte) temp;
+        bitBuffer = bitBuffer >>> 8;
+        bitBufferLen -= 8;
+        // before adding the backRef data pair to the dictionary, check if it is full, and resize if so
+        if (index == indexDictionary.length) {
+          int[] tempArray = new int[indexDictionary.length * 2];
+          System.arraycopy(indexDictionary, 0, tempArray, 0, indexDictionary.length);
+          indexDictionary = tempArray;
+          byte[] tempArray2 = new byte[dataDictionary.length * 2];
+          System.arraycopy(dataDictionary, 0, tempArray2, 0, dataDictionary.length);
+          dataDictionary = tempArray2;
+        }
+        //save new backRef and data pair to the dictionary at the current index location
+        indexDictionary[index] = backRef;
+        dataDictionary[index] = data;
+        // follow the sequence of backRefs until it hits 0 (the root)
+        // pushing each data byte from each index onto a stack so that it is ordered correctly
+        byteSequence.push(data);
+        while (backRef != 0) {
+          byteSequence.push(dataDictionary[backRef]);
+          backRef = indexDictionary[backRef];
+        }
+        // write from the byte stack until it is empty
+        while (!byteSequence.empty()) {
+          System.out.write(byteSequence.pop().byteValue());
+        }
+        // if the next index requires another bit of information, then increment bitsToEncode
+        if (++index == encodeAnotherBitOnThisIndex) {
+          encodeAnotherBitOnThisIndex *= 2;
+          bitsToEncode++;
+        }
       }
     }
     System.out.flush();
-  }
-
-  private static int readInt(int firstByte) throws IOException {
-    // TODO bitunpacking
-    // returns the int index in big endian
-    return (firstByte << 24 | System.in.read() << 16 | System.in.read() << 8 | System.in.read());
   }
 }
