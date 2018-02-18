cbbcc367030a6d585e0c55f85fed53d3de57be9b
Brad Hansen <brad.m.hansen@gmail.com>
2018-02-18 19:01:58 +1300

    Format changes

--- ArrayTrie.java
+++ ArrayTrie.java
@@ -4,18 +4,18 @@ public class ArrayTrie {
   private ArrayTrie[] children = new ArrayTrie[256];
 
   public int getValue(byte pos) {
-    return value[pos&255];
+    return value[pos & 255];
   }
 
   public void setValue(byte pos, int newValue) {
-    value[pos&255] = newValue;
+    value[pos & 255] = newValue;
   }
 
   public ArrayTrie getChild(byte pos) {
-    return children[pos&255];
+    return children[pos & 255];
   }
 
   public void setChild(byte pos, ArrayTrie newChild) {
-    children[pos&255] = newChild;
+    children[pos & 255] = newChild;
   }
 }
