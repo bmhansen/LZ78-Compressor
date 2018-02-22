00d644e38b7ffba0ea764af5bf535396aadf86a6
Brad Hansen <brad.m.hansen@gmail.com>
2018-02-22 22:16:08 +1300

    Separates bitunpacking from the decompressor

---
+++ BitUnpacker.java
@@ -0,0 +1,64 @@
+public class BitUnpacker {
+
+  // bitpacked data will be read into and will queue up in this buffer
+  // a long was chosen because it needs to hold a byte, (8 bits) plus a large int (up to 32bits)  
+  // plus any left over bits not flushed and waiting to be read (up to 7)
+  // it's a first-in first-out queue where it builds from the least significant bit
+  long bitBuffer = 0;
+  // the number of bits in use in the buffer
+  int bitsInUse = 0;
+  long extractionBuffer;
+
+  // number of bits needed to minimally decode the backRef
+  int minBitsToDecode = 0;
+  // the number of writes that have occured (also the max possible backRef)
+  int numberOfReads = 0;
+  // update minBitsToDecode when this matches numberOfReads
+  int decodeAnotherBitOn = 1;
+
+  // backRef and dataByte pair extracted from unpacking a stream of bytes
+  int backRef = 0;
+  byte dataByte = 0;
+
+  // reads the input until it can unpack a backRef, data byte pair
+  public boolean read() {
+    for (int inputInt = System.in.read(); inputInt >= 0; inputInt = System.in.read()) {
+      // add them to the buffer
+      bitBuffer = bitBuffer | (long) inputInt << bitsInUse;
+      bitsInUse += 8;
+      // when the buffer contains the ref and data, extract them
+      if (bitsInUse >= minBitsToDecode + 8) {
+        // isolate the reference from the buffer
+        extractionBuffer = (bitBuffer << 64 - minBitsToDecode) >>> 64 - minBitsToDecode;
+        backRef = (int) extractionBuffer;
+        bitBuffer >>>= minBitsToDecode;
+        bitsInUse -= minBitsToDecode;
+        // isolate the data byte from the buffer
+        extractionBuffer = (bitBuffer << 64 - 8) >>> 64 - 8;
+        data = (byte) extractionBuffer;
+        bitBuffer >>>= 8;
+        bitsInUse -= 8;
+
+        numberOfReads++;
+        // if the next backRef requires another bit of information, then increment minBitsToDecode
+        if (numberOfReads == decodeAnotherBitOn) {
+          minBitsToDecode++;
+          decodeAnotherBitOn *= 2;
+        }
+
+        // backRef and the dataByte have been unpacked, time to return successfully
+        return true;
+      }
+    }
+    // end of input, return with a failure to gather a new backRef and dataByte
+    return false;
+  }
+
+  public int getBackRef() {
+    return backRef;
+  }
+
+  public byte getDataByte() {
+    return dataByte;
+  }
+}
--- Decompressor.java
+++ Decompressor.java
@@ -11,60 +11,39 @@ public class Decompressor {
     int[] indexDictionary = new int[256];
     byte[] dataDictionary = new byte[256];
     int index = 1;
-    int encodeAnotherBitOnThisIndex = 2;
-    int bitsToEncode = 1;
     int backRef = 0;
-    byte data = 0;
+    byte dataByte = 0;
     Stack<Byte> byteSequence = new Stack<Byte>();
-    long bitBuffer = 0;
-    int bitBufferLen = 0;
-    long temp = 0;
+
+    BitUnpacker bu = new BitUnpacker();
 
     // while there are bytes to read
-    for (int inputInt = System.in.read(); inputInt >= 0; inputInt = System.in.read()) {
-      // add them to the buffer
-      bitBuffer = bitBuffer | (long) inputInt << bitBufferLen;
-      bitBufferLen += 8;
-      // when the buffer contains the ref and data, extract them
-      if (bitBufferLen >= bitsToEncode + 8) {
-        // isolate the reference from the buffer
-        temp = (bitBuffer << 64 - bitsToEncode) >>> 64 - bitsToEncode;
-        backRef = (int) temp;
-        bitBuffer = bitBuffer >>> bitsToEncode;
-        bitBufferLen -= bitsToEncode;
-        // isolate the data byte from the buffer
-        temp = (bitBuffer << 64 - 8) >>> 64 - 8;
-        data = (byte) temp;
-        bitBuffer = bitBuffer >>> 8;
-        bitBufferLen -= 8;
-        // before adding the backRef data pair to the dictionary, check if it is full, and resize if so
-        if (index == indexDictionary.length) {
-          int[] tempArray = new int[indexDictionary.length * 2];
-          System.arraycopy(indexDictionary, 0, tempArray, 0, indexDictionary.length);
-          indexDictionary = tempArray;
-          byte[] tempArray2 = new byte[dataDictionary.length * 2];
-          System.arraycopy(dataDictionary, 0, tempArray2, 0, dataDictionary.length);
-          dataDictionary = tempArray2;
-        }
-        //save new backRef and data pair to the dictionary at the current index location
-        indexDictionary[index] = backRef;
-        dataDictionary[index] = data;
-        // follow the sequence of backRefs until it hits 0 (the root)
-        // pushing each data byte from each index onto a stack so that it is ordered correctly
-        byteSequence.push(data);
-        while (backRef != 0) {
-          byteSequence.push(dataDictionary[backRef]);
-          backRef = indexDictionary[backRef];
-        }
-        // write from the byte stack until it is empty
-        while (!byteSequence.empty()) {
-          System.out.write(byteSequence.pop().byteValue());
-        }
-        // if the next index requires another bit of information, then increment bitsToEncode
-        if (++index == encodeAnotherBitOnThisIndex) {
-          encodeAnotherBitOnThisIndex *= 2;
-          bitsToEncode++;
-        }
+    while (bu.read()) {
+      // gather the backRef dataByte pairs
+      backRef = bu.getBackRef();
+      dataByte = bu.getDataByte();
+      // before adding the backRef dataByte pair to the dictionary, check if it is full, and resize if so
+      if (index == indexDictionary.length) {
+        int[] tempArray = new int[indexDictionary.length * 2];
+        System.arraycopy(indexDictionary, 0, tempArray, 0, indexDictionary.length);
+        indexDictionary = tempArray;
+        byte[] tempArray2 = new byte[dataDictionary.length * 2];
+        System.arraycopy(dataDictionary, 0, tempArray2, 0, dataDictionary.length);
+        dataDictionary = tempArray2;
+      }
+      //save new backRef and dataByte pair to the dictionary at the current index location
+      indexDictionary[index] = backRef;
+      dataDictionary[index] = dataByte;
+      // follow the sequence of backRefs until it hits 0 (the root)
+      // pushing each dataByte from each index onto a stack so that it is ordered correctly
+      byteSequence.push(dataByte);
+      while (backRef != 0) {
+        byteSequence.push(dataDictionary[backRef]);
+        backRef = indexDictionary[backRef];
+      }
+      // write from the byte stack until it is empty
+      while (!byteSequence.empty()) {
+        System.out.write(byteSequence.pop().byteValue());
       }
     }
     System.out.flush();
