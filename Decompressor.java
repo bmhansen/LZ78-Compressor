6048d7a3a13bfb611068b81dd89a536c557413bd
Brad Hansen <brad.m.hansen@gmail.com>
2018-02-25 18:25:26 +1300

    Adds an arraylist to handle the dictionary and adds comments

--- Decompressor.java
+++ Decompressor.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.Stack;
+import java.util.ArrayList;
 
 public class Decompressor {
   public static void main(String[] args) throws IOException {
@@ -7,45 +8,44 @@ public class Decompressor {
   }
 
   public static void LZ78Decode() throws IOException {
-    // initial setup
-    int[] indexDictionary = new int[256];
-    byte[] dataDictionary = new byte[256];
-    int index = 1;
-    int backRef = 0;
-    byte dataByte = 0;
-    Stack<Byte> byteSequence = new Stack<Byte>();
+    // Initial setup
 
+    // The dictionary of back reference values
+    ArrayList<Integer> backRefDictionary = new ArrayList<Integer>();
+    // The dictionary of byte values which correspond to the back references at the same position
+    ArrayList<Byte> byteDictionary = new ArrayList<Byte>();
+    // The back reference gathered from input
+    int inputBackRef = 0;
+    // The data byte gathered from input
+    byte inputDataByte = 0;
+    // The sequence of bytes to be written is stored in a stack to help reverse their order
+    Stack<Byte> byteSequence = new Stack<Byte>();
+    // An instance of the BitUnpacker class is created to be used in bit unpacking the input
     BitUnpacker bu = new BitUnpacker();
 
     // while there are bytes to read
     while (bu.read()) {
-      // gather the backRef dataByte pairs
-      backRef = bu.getBackRef();
-      dataByte = bu.getDataByte();
-      // before adding the backRef dataByte pair to the dictionary, check if it is full, and resize if so
-      if (index == indexDictionary.length) {
-        int[] tempArray = new int[indexDictionary.length * 2];
-        System.arraycopy(indexDictionary, 0, tempArray, 0, indexDictionary.length);
-        indexDictionary = tempArray;
-        byte[] tempArray2 = new byte[dataDictionary.length * 2];
-        System.arraycopy(dataDictionary, 0, tempArray2, 0, dataDictionary.length);
-        dataDictionary = tempArray2;
-      }
-      //save new backRef and dataByte pair to the dictionary at the current index location
-      indexDictionary[index] = backRef;
-      dataDictionary[index] = dataByte;
-      // follow the sequence of backRefs until it hits 0 (the root)
-      // pushing each dataByte from each index onto a stack so that it is ordered correctly
-      byteSequence.push(dataByte);
-      while (backRef != 0) {
-        byteSequence.push(dataDictionary[backRef]);
-        backRef = indexDictionary[backRef];
+      // gather the back reference, data byte pairs
+      inputBackRef = bu.getBackRef();
+      inputDataByte = bu.getDataByte();
+      // save the new backRef and dataByte pair to the dictionary
+      backRefDictionary.add(inputBackRef);
+      byteDictionary.add(inputDataByte);
+
+      // push the input data byte onto the stack to be output
+      byteSequence.push(inputDataByte);
+      // follow the sequence of back references until it hits 0 (the root)
+      while (inputBackRef != 0) {
+        // pushing each data byte from each back reference onto a stack
+        byteSequence.push(byteDictionary.get(inputBackRef));
+        inputBackRef = backRefDictionary.get(inputBackRef);
       }
       // write from the byte stack until it is empty
       while (!byteSequence.empty()) {
         System.out.write(byteSequence.pop().byteValue());
       }
     }
+    // flush in case any bytes have not been sent on the output
     System.out.flush();
   }
 }
