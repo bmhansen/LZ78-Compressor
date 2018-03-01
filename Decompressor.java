4570487ba865ba2e46991f887c126240b82094ee
Brad Hansen <brad.m.hansen@gmail.com>
2018-03-01 21:25:58 +1300

    Changes the decompressor to take an input and output stream

--- Decompressor.java
+++ Decompressor.java
@@ -1,13 +1,15 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Stack;
 import java.util.ArrayList;
 
 public class Decompressor {
   public static void main(String[] args) throws IOException {
-    LZ78Decode();
+    LZ78Decode(System.in, System.out);
   }
 
-  public static void LZ78Decode() throws IOException {
+  public static void LZ78Decode(InputStream in, OutputStream out) throws IOException {
     // Initial setup
 
     // The dictionary of back reference values
@@ -21,7 +23,7 @@ public class Decompressor {
     // The sequence of bytes to be written is stored in a stack to help reverse their order
     Stack<Byte> byteSequence = new Stack<Byte>();
     // An instance of the BitUnpacker class is created to be used in bit unpacking the input
-    BitUnpacker bu = new BitUnpacker();
+    BitUnpacker bu = new BitUnpacker(in);
 
     // while there are bytes to read
     while (bu.read()) {
@@ -37,15 +39,15 @@ public class Decompressor {
       // follow the sequence of back references until it hits 0 (the root)
       while (inputBackRef != 0) {
         // pushing each data byte from each back reference onto a stack
-        byteSequence.push(byteDictionary.get(inputBackRef));
-        inputBackRef = backRefDictionary.get(inputBackRef);
+        byteSequence.push(byteDictionary.get(inputBackRef - 1));
+        inputBackRef = backRefDictionary.get(inputBackRef - 1);
       }
       // write from the byte stack until it is empty
       while (!byteSequence.empty()) {
-        System.out.write(byteSequence.pop().byteValue());
+        out.write(byteSequence.pop().byteValue());
       }
     }
     // flush in case any bytes have not been sent on the output
-    System.out.flush();
+    out.flush();
   }
 }
